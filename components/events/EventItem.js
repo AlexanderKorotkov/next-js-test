dc84384d8f7d777f96e818eec6df4c8bb5a0600b
alexander.korotkov <manderlai87@gmail.com>
2021-10-29 17:02:43 +0300

    init commit

---
+++ components/events/EventItem.js
@@ -0,0 +1,36 @@
+import Link from 'next/link';
+import React from 'react';
+
+function EventItem(props) {
+  const { title, image, data, location, id } = props;
+
+  const humanReadableDate = new Date(data).toDateString('en-US', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  });
+  const formattedAddress = location.replace(', ', '\n');
+  const exploreLink = `/event/${id}`;
+
+  return (
+    <li>
+      <img src={'/' + image} alt='' />
+      <div>
+        <div>
+          <h2>{title}</h2>
+          <div>
+            <time>{humanReadableDate}</time>
+          </div>
+          <div>
+            <address>{formattedAddress}</address>
+          </div>
+        </div>
+        <div>
+          <Link href={exploreLink}>Explore Event</Link>
+        </div>
+      </div>
+    </li>
+  );
+}
+
+export default EventItem;
---
+++ pages/events/[...slug].js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function FilteredEventsPage() {
+  return <div></div>;
+}
+
+export default FilteredEventsPage;
